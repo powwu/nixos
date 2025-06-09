9a4088d9985cca452e29c9ae7ba450df61d5692e
powwu <hazy@vivaldi.net>
2025-06-09 12:51:28 -0700

    make sure laptop.nix is always commented when updating repo

--- update-repo.sh
+++ update-repo.sh
@@ -5,6 +5,7 @@ git clone https://github.com/powwu/nixos /tmp/nixos || exit
 cd /tmp/nixos
 rm -rf ./*
 cp -r /etc/nixos/* .
+sed -i 's|^\([[:space:]]*\)\([^#][[:space:]]*./extra/laptop.nix[[:space:]]*\)|\1# \2|' flake.nix
 git add -u .
 git commit -m "$(date --iso-8601=s)"
 git push
