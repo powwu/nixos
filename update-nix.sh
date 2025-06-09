0a9cb52d6396ef0620091a682a828cddcd507f5d
powwu <hazy@vivaldi.net>
2025-06-09 12:22:04 -0700

    2025-06-09
    2025-06-09
    
    2025-06-09
    
    2025-06-09
    
    2025-06-09
    
    2025-06-09

--- update-nix.sh
+++ update-nix.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env sh
 
-cp -r /etc/nixos /etc/nixos-bak-$(date --iso-8601)
-rm -rf /etc/nixos/
-git clone https://github.com/powwu/nixos /etc/nixos/
-nixos-generate-config
-rm -rf /etc/nixos/.git
-rm -rf /etc/nixos/configuration.nix
-mv /etc/nixos/hardware-configuration.nix /etc/nixos/nixos/
+[ "$(pwd)" = "/etc/nixos" ] && cd /tmp
+
+(
+  cp -r /etc/nixos "/etc/nixos-bak-$(date --iso-8601)"
+  rm -rf /etc/nixos/
+  git clone https://github.com/powwu/nixos /etc/nixos/
+  nixos-generate-config
+  rm -rf /etc/nixos/.git
+  rm -f /etc/nixos/configuration.nix
+  mv /etc/nixos/hardware-configuration.nix /etc/nixos/nixos/
+)
+
+exec env -i HOME="$HOME" PATH="$PATH" /bin/sh -c 'cd /etc/nixos && exec "$SHELL"'
--- update-repo.sh
+++ update-repo.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+rm -rf /tmp/nixos
 git clone https://github.com/powwu/nixos /tmp/nixos || exit
 cd /tmp/nixos
 rm -rf ./*
