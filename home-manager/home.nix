a1711bb479b3ab00882937ad145a5cc8f541d54e
powwu <hazy@vivaldi.net>
2025-06-12 15:57:03 -0700

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -149,7 +149,7 @@ in {
       spotify = "spicetify watch -s";
       unfav = "cat ~/.current-wallpaper | rev | cut -d '/' -f 1 | rev | xargs -I {} rm ~/Wallpapers/wallpapers/favorites/{}";
       lnp = "export NIX_PATH='nixpkgs=/home/james/nixpkgs/'";
-      q = "amazon-q"
+      q = "amazon-q";
     };
     history.size = 1000000;
     history.path = "/home/james/.histfile";
