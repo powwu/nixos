2fabbae07050744740101e0ed61780043ed82492
powwu <hello@powwu.sh>
2025-06-13 07:02:28 -0700

    2025-06-13T07:02:28-07:00

--- flake.nix
+++ flake.nix
@@ -64,7 +64,8 @@
 
     homeConfigurations = {
       "james@powwuinator" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${system};
+        # pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
           ./home-manager/home.nix
