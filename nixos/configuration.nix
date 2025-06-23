0ec3c5b47caab797fd59f8f6cdc8440c2e9442b0
powwu <hello@powwu.sh>
2025-06-23 10:42:05 -0700

    2025-06-23T10:42:05-07:00

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -121,7 +121,7 @@
   programs.firefox = {
     enable = true;
     preferences = {
-      "widget.use-xdg-desktop-portal.file-picker" = 1;
+    #  "widget.use-xdg-desktop-portal.file-picker" = 1;
     };
   };
 
