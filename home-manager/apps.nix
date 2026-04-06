b8856d54241fdc5946bb160aeec61185e3d154a5
powwu <hello@powwu.sh>
2026-04-06 11:52:13 -0400

    2026-04-06T11:52:13-04:00

--- home-manager/apps.nix
+++ home-manager/apps.nix
@@ -274,7 +274,7 @@ in {
   # overwriting would be a cause for concern. however, home-manager makes sure that any backups are not overwritten, and will refuse to continue if that's not the case
   home.file.".spacemacs".source = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/powwu/dotspacemacs/refs/heads/main/.spacemacs";
-    hash = "sha256-+ZWu2MUZspDsXnDKuG1X6fUkpOYKXPsC0ttSD8Gil80=";
+    hash = "sha256-nnIIXHscEPoUFtplsoGBcuWiZ3EkvNiHb7keqSQ6t7A=";
   };
 
   home.activation = {
